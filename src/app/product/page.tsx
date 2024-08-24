069d80ba503e98f8040f6f1f3f6f430765c089a2
Emre Gulec <emregulec@MacBook-Pro.local>
2024-08-24 14:46:16 +0300

    Add products app

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,52 +1,36 @@
 import { getProduct } from "@/services/product";
 import ProductScreen from "src/screens/ProductScreen/index";
 
-const ProductPage = async (context: { searchParams: any }) => {
+const buildUrlParams = (searchParams) => {
+  const {
+    sayfa: page,
+    ara: search,
+    sirala: sortby,
+    sira: order,
+    marka: brand,
+    model,
+  } = searchParams;
+
+  const orderDirection = order === "artan" ? "asc" : "desc";
+  const sortDirection =
+    sortby === "tarih" ? "createdAt" : sortby === "fiyat" ? "price" : "";
+
+  const urlParams = new URLSearchParams();
+  if (page) urlParams.append("page", page);
+  if (search) urlParams.append("search", search);
+  if (sortby) urlParams.append("sortby", sortDirection);
+  if (order) urlParams.append("order", orderDirection);
+  if (brand) urlParams.append("brand", brand);
+  if (model) urlParams.append("model", model);
+  urlParams.append("limit", "12");
+
+  return urlParams.toString();
+};
+
+const ProductPage = async (context) => {
   try {
     const { searchParams } = context;
-    const {
-      sayfa: page,
-      ara: search,
-      sirala: sortby,
-      sira: order,
-      marka: brand,
-      model,
-    } = searchParams;
-
-    const orderDirection = order === "artan" ? "asc" : "desc";
-    const sortDirection =
-      sortby === "tarih"
-        ? "createdAt"
-        : sortby === "fiyat"
-        ? "price"
-        : "";
-
-    const urlParams = new URLSearchParams();
-    if (page) {
-      urlParams.append("page", page);
-      urlParams.append("limit", "12");
-    }
-
-    if (search) {
-      urlParams.append("search", search);
-    }
-
-    if (sortby) {
-      urlParams.append("sortby", sortDirection);
-    }
-
-    if (order) {
-      urlParams.append("order", orderDirection);
-    }
-
-    if (brand) {
-      urlParams.append("brand", brand);
-    }
-
-    if (model) {
-      urlParams.append("model", model);
-    }
-    const url = `?${urlParams.toString()}`;
+    const url = `?${buildUrlParams(searchParams)}`;
 
     const response = await getProduct(url);
     const pageResponse = {
--- src/services/product.js
+++ src/services/product.js
@@ -1,10 +1,7 @@
 export const getProduct = async (param) => {
-  const url = `https://5fc9346b2af77700165ae514.mockapi.io/products${param}`;
-  console.log("url111",url)
   try {
-    const response = await fetch(url);
+    const response = await fetch(`https://5fc9346b2af77700165ae514.mockapi.io/products${param}`);
     const data = await response.json();
-    console.log("data",data)
     return data;
   } catch (error) {
     console.error("error", error);
